f45e8b8fe77aeec7fdc59bff6a6d9a3209a9877b
chetan-mrk <108858458+chetan-mrk@users.noreply.github.com>
2022-07-07 14:49:38 +0530

    Add files via upload

---
+++ Circle.js
@@ -0,0 +1,9 @@
+function Circle(){
+	var rad = document.getElementById('n1').value;
+	document.getElementById('n2').value=3.14*rad;
+	document.getElementById('n3').value=3.14*2*rad;
+}
+function Sqaure(){
+	var side = document.getElementById('s1').value;
+	document.getElementById('s2').value=side*side;
+}
