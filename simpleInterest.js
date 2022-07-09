d495d155d4eea23a65ef06a80b06a548ab53246b
chetan-mrk <108858458+chetan-mrk@users.noreply.github.com>
2022-07-09 12:36:09 +0530

    Delete simpleInterest.js

--- simpleInterest.js
+++
@@ -1,7 +0,0 @@
-function smpint(){
-   var p = document.getElementById('n1').value;
-   var t = document.getElementById('n2').value;
-   var r = document.getElementById('n3').value;
-   var ptr = parseInt(p)*parseInt(t)*parseInt(r)/100;
-   document.getElementById('n4').value=ptr;
-}
