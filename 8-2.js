4f856eaac0de773b92eb2ff7f88ddfaeb75a2c88
chetan-mrk <108858458+chetan-mrk@users.noreply.github.com>
2022-07-11 10:12:52 +0530

    Table Added
    Table Added

--- 8-2.js
+++ 8-2.js
@@ -1,10 +1,12 @@
 function tab(){
 	var num1 = document.getElementById('n1').value;
 	var num2 = document.getElementById('n2').value;
-	var num3;
-	for(var a=1; a<=num2; a++){
-		num3 = num1 + ' x '+num2+ ' = '+(num1*num2);
+	var num3 ='<table>';
+	var str = '<table class="table hover"><tbody>';
+	for(a=parseInt(num1); a<=parseInt(num2); a++){
+		num3 = num3 +'<tr><td>' +(a*1)+'</td><td>'+(a*2)+'</td><td>'+(a*3)+'</td></tr>'+(a*4)+'</td></tr>';
 	}
+	num3 = num3+'<table>';
 	document.getElementById('s1').innerHTML=num3;
 
 }
